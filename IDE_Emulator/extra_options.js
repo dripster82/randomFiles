63cb43d8104cc710d1f13afd476078dd5519785c
Paul Ketelle <paul.ketelle@builder.ai>
2023-07-03 17:41:17 +0100

    adding js

---
+++ IDE_Emulator/extra_options.js
@@ -0,0 +1,46 @@
+<script>
+    $(function() {
+		launchPageConfig["apps"][0]["iOS"]["devices"] = [
+			{
+				"name": "iPhone 8 (iOS 13.7)",
+				"type": "iphone8",
+				"osVersion": "13.7"
+			},
+			{
+				"name": "iPhone 8 plus (iOS 13.7)",
+				"type": "iphone8plus",
+				"osVersion": "13.7"
+			},
+			{
+				"name": "iPhone 11 pro (iOS 14.5)",
+				"type": "iphone11pro",
+				"osVersion": "14.5"
+			},
+			{
+				"name": "iPhone 12 (iOS 14.5)",
+				"type": "iphone12",
+				"osVersion": "14.5"
+			},
+			{
+				"name": "iPhone 13 Pro (iOS 15.5)",
+				"type": "iphone13pro",
+				"osVersion": "15.5"
+			},
+			{
+				"name": "iPhone 14 Pro  (iOS 16.2)",
+				"type": "iphone14pro",
+				"osVersion": "16.2"
+			},
+			{
+				"name": "iPhone 14 Pro Max (iOS 16.2)",
+				"type": "iphone14promax",
+				"osVersion": "16.2"
+			},
+			{
+				"name": "iPad Air 4th Gen (iOS 16.2)",
+				"type": "ipadair4thgeneration",
+				"osVersion": "16.2"
+			},
+	    	];
+	});
+</script>
